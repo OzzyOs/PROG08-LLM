59c2cc76f5aaa969aa3126312818edc35c9b074f
OzzyOs <Joshahursepuny@gmail.com>
2024-05-22 20:09:36 +0200

    feature-3/Molukse-Koffer: add :
    -add misc files.

---
+++ MolukseKoffer/app/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      theme: {
+        height: {
+          100 : '50rem'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
